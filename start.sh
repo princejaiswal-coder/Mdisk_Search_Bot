a860a54c63d0fb3d94492571a3cb71342dd8a8f4
Prince Jaiswal <princejaiswal7123@gmail.com>
2023-01-07 20:36:24 +0530

    none

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/princejaiswal-coder/Mdisk_Search_Bot /Mdisk_Search_Bot
-cd /Mdisk_Search_Bot
+git clone https://github.com/princejaiswal-coder/Mdisk_Search_Bot /MDISK_SEARCH_BOT
+cd /MDISK_SEARCH_BOT
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
