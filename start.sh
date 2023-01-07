c17844eed7224c6247abf10251f8cc3e737686d0
Prince Jaiswal <princejaiswal7123@gmail.com>
2023-01-07 20:39:08 +0530

    test

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ git clone https://github.com/princejaiswal-coder/Mdisk_Search_Bot /MDISK_SEARCH_
 cd /MDISK_SEARCH_BOT
 pip3 install -r requirements.txt
 echo "Starting Bot...."
-python3 main.py
+python main.py
